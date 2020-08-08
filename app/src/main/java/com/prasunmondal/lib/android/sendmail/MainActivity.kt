aa18042c6ded4318cfbd5a0362723a4af6b9e8ee
Prasun <pmandal@akamai.com>
2020-08-08 21:37:53 +0530

    all working good

--- app/src/main/java/com/prasunmondal/lib/android/sendmail/MainActivity.kt
+++ app/src/main/java/com/prasunmondal/lib/android/sendmail/MainActivity.kt
@@ -3,10 +3,31 @@ package com.prasunmondal.lib.android.sendmail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SendMailTrigger().sendMessage("prsn.online@gmail.com",
+            "bpuxonmqiwokunaf",
+            listOf<String>("prasun.mondal02@gmail.com").toTypedArray(),
+            "SubjectTest",
+            "Content",
+            findViewById(R.id.textview),
+            "Sending Bill...",
+            "Bill Sent.",
+            false)
+
+        SendMailTrigger().sendMessage("prsn.online@gmail.com",
+            "bpuxonmqiwokunaf",
+            listOf("prasun.mondal02@gmail.com").toTypedArray(),
+            "SubjectTest",
+            "<h1>Content</h1>",
+            findViewById(R.id.textview),
+            "Sending Bill...",
+            "Bill Sent.",
+            true)
     }
 }
